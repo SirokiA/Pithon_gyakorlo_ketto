49c51d29a2cce89dcf21b7a009ac812014713e56
SirokiA <sirokia@kkszki.hu>
2025-12-04 16:36:07 +0100

    Harmadik feladat

--- gyakorlas2.py
+++ gyakorlas2.py
@@ -11,11 +11,29 @@
 #print("A(z)", szam, "faktoriálisa:", fakt)
 
 #Második feladat
-n = int(input("Adj meg egy N számot: "))
+#n = int(input("Adj meg egy N számot: "))
 
-szorzat = 1
+#szorzat = 1
 
-for i in range(1, n + 1):
-    szorzat *= i
+#for i in range(1, n + 1):
+#    szorzat *= i
 
-print(f"Az 1-től {n}-ig terjedő számok szorzata: {szorzat}")
+#print(f"Az 1-től {n}-ig terjedő számok szorzata: {szorzat}")
+
+#Harmadik feladat
+
+szam = int(input("Adj meg egy számot: "))
+
+if szam <= 1:
+    print("Ez a szám nem prímszám.")
+else:
+    prim = True
+    for i in range(2, szam):
+        if szam % i == 0:
+            prim = False
+            break
+
+    if prim:
+        print("A szám prímszám.")
+    else:
+        print("A szám nem prímszám.")
