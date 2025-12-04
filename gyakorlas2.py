0f051d4fbdc71ef9d3ec957fdc97c5e98ee6a9da
SirokiA <sirokia@kkszki.hu>
2025-12-04 16:28:50 +0100

    Masodik feladat

--- gyakorlas2.py
+++ gyakorlas2.py
@@ -1,10 +1,21 @@
-szam = int(input("Adj meg egy számot: "))
+#Első feladat
+#szam = int(input("Adj meg egy számot: "))
 
-fakt = 1
-i = 1
+#fakt = 1
+#i = 1
 
-while i <= szam:
-    fakt *= i
-    i += 1
+#while i <= szam:
+#    fakt *= i
+#    i += 1
 
-print("A(z)", szam, "faktoriálisa:", fakt)
+#print("A(z)", szam, "faktoriálisa:", fakt)
+
+#Második feladat
+n = int(input("Adj meg egy N számot: "))
+
+szorzat = 1
+
+for i in range(1, n + 1):
+    szorzat *= i
+
+print(f"Az 1-től {n}-ig terjedő számok szorzata: {szorzat}")
