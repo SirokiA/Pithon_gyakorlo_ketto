24b972f4756cded7e8fb7d93a6ca6dc60ef1c2e8
SirokiA <sirokia@kkszki.hu>
2025-12-04 16:40:10 +0100

    Negyedik feladat

--- gyakorlas2.py
+++ gyakorlas2.py
@@ -22,18 +22,30 @@
 
 #Harmadik feladat
 
-szam = int(input("Adj meg egy számot: "))
+#szam = int(input("Adj meg egy számot: "))
 
-if szam <= 1:
-    print("Ez a szám nem prímszám.")
-else:
-    prim = True
-    for i in range(2, szam):
-        if szam % i == 0:
-            prim = False
-            break
+#if szam <= 1:
+#    print("Ez a szám nem prímszám.")
+#else:
+#    prim = True
+#    for i in range(2, szam):
+#        if szam % i == 0:
+#            prim = False
+#            break
 
-    if prim:
-        print("A szám prímszám.")
-    else:
-        print("A szám nem prímszám.")
+#    if prim:
+#        print("A szám prímszám.")
+#    else:
+#        print("A szám nem prímszám.")
+
+
+#Negyedik feladat
+
+szam= int(input("Adj meg egy számot: "))
+
+osszeg=0
+
+for i in str(abs(szam)):
+    osszeg += int(i)
+
+print(f"A(z) {szam} szám számjegyeinek összege: {osszeg}")
